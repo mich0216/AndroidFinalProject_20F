96749b4c96382f86cdc10e27db5c829aebbe27e4
mich0216 <mich0216@algonquinlive.com>
2020-11-25 18:23:50 -0500

    done milestone-2

--- app/src/main/java/com/example/androidfinalproject_20f/sabiha/EventSearchActivity.java
+++ app/src/main/java/com/example/androidfinalproject_20f/sabiha/EventSearchActivity.java
@@ -4,12 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.androidfinalproject_20f.MainActivity;
 import com.example.androidfinalproject_20f.R;
 
 public class EventSearchActivity extends AppCompatActivity {
