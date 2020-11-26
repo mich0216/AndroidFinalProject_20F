f17aad9410ec022e4a2ac3dd92799afb24bbf07c
vnaa2 <Aharan 2009>
2020-11-25 21:59:17 -0500

    Milestone 2 completed - audio search

--- app/src/main/java/com/example/androidfinalproject_20f/audiosearch/ArtistInputActivity.java
+++ app/src/main/java/com/example/androidfinalproject_20f/audiosearch/ArtistInputActivity.java
@@ -2,20 +2,21 @@ package com.example.androidfinalproject_20f.audiosearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.androidfinalproject_20f.MainActivity;
 import com.example.androidfinalproject_20f.R;
+
 /**
  * @author Vettivale Ponnampalam
  * CST 2335 -020
  * The main class for the ArtistInputActivity extended from AppCompatActivity
- * */
+ */
 public class ArtistInputActivity extends AppCompatActivity {
 
     /**
@@ -28,6 +29,8 @@ public class ArtistInputActivity extends AppCompatActivity {
      */
     private Button searchButton;
 
+    private SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +39,22 @@ public class ArtistInputActivity extends AppCompatActivity {
         albumNameEditText = findViewById(R.id.albumNameEditText);
         searchButton = findViewById(R.id.searchButton);
 
+        sp = getSharedPreferences("AudioSearchSharedPreference", Context.MODE_PRIVATE);
+
+        albumNameEditText.setText(sp.getString("ARTIST_NAME", ""));
+
         searchButton.setOnClickListener(v -> {
             String artistName = albumNameEditText.getText().toString();
 
             if (artistName.isEmpty()) {
 
-                Toast.makeText(this, "Please enter the artist name", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, getString(R.string.as_please_enter_album_name), Toast.LENGTH_SHORT).show();
 
             } else {
+
+                SharedPreferences.Editor e = sp.edit();
+                e.putString("ARTIST_NAME", artistName);
+                e.commit();
 
                 Intent i = new Intent(ArtistInputActivity.this, AlbumListActivity.class);
                 i.putExtra("ARTIST_NAME", artistName);
